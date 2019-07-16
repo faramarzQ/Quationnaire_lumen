dbd12a6c6d21718a51d3414c600fdef815baf2a2
Parviz Ansaryan <parviz.ansaryan@gmail.com>
2019-07-16 12:15:37 +0430

    add appuser & verification migration

---
+++ database/migrations/2019_07_16_093505_create_appuser_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppuserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appuser', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('type', ['app_user', 'questioner']);
+            $table->string('name');
+            $table->integer('mobile');
+            $table->string('password');
+            $table->string('api_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appuser');
+    }
+}
---
+++ database/migrations/2019_07_16_094127_create_verification_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVerificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('verify_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification');
+    }
+}
